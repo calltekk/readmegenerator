9a2a4de22c9dbd128d81aaf01f361ca872399642
calltekk <callumgghilton@outlook.com>
2024-02-20 19:41:07 +0000

    added starter code, index additions

--- index.js
+++ index.js
@@ -1,21 +1,139 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
+const util = require("util");
+const emailValidator = require("email-validator");
+
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+const writeFileAsync = util.promisify(fs.writeFile);
 
+// Function to validate user email address
+const validateEmail = (email) => {
+  if (emailValidator.validate(email)) {
+    return true;
+  } else {
+    return "Please enter a valid email address.";
+  }
+};
+
+// Array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "username",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+    validate: validateEmail,
+  },
+  {
+    type: "input",
+    name: "project",
+    message: "What is your project's name?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message:
+      "What kind of license should your project have? Please pick one of the licenses from the list or select None if you would like no licence.",
+    choices: [
+      "Academic",
+      "Apache",
+      "Artistic",
+      "Boost-1.0",
+      "BSD-2-Clause",
+      "BSD_3_Clause",
+      "BSD_3_Clause_Clear",
+      "Creative",
+      "Creative_Attribution",
+      "Educational",
+      "Eclipse",
+      "GPL",
+      "LGPL",
+      "ISC",
+      "LaTeX",
+      "Microsoft",
+      "MIT",
+      "Mozilla",
+      "OSL",
+      "PostgreSQL",
+      "NCSA",
+      "Unlicense",
+      "zLib",
+      "None",
+    ],
+  },
+  {
+    type: "input",
+    name: "dependencies",
+    message: "What command should be run to install dependencies",
+    default: "`npm i`",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What command should be run to run tests",
+    default: "`npm test`",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "What does the user need to know about contributing to the repo?",
+  },
+  {
+    type: "input",
+    name: "credits",
+    message:
+      "List your collaborators, if any, with links to their GitHub profiles.",
+  },
+  {
+    type: "checkbox",
+    name: "badges",
+    message:
+      "Please select which languages and tools you used to create this project.",
+    choices: ["HTML", "CSS", "Bootstrap", "JavaScript", "jQuery", "NodeJS"],
+  },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+// Function to prompt user
+const promptUser = () => {
+  return inquirer.prompt(questions);
+};
 
-// function to initialize program
-function init() {
+// Function to write to README
+const init = async () => {
+  try {
+    const answers = await promptUser();
+    const markdown = generateMarkdown(answers);
 
-}
+    if (!fs.existsSync(`generated`)) {
+      fs.mkdirSync(`generated`);
+    }
 
-// function call to initialize program
+    await writeFileAsync(
+      path.join(`${process.cwd()}/generated/`, "README.md"),
+      markdown
+    );
+
+    console.info("Successfully wrote to README.md");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,61 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const { generateBadges, generateLicenseBadge } = require("./generateBadges");
 
-`;
-}
+// Function to generate markdown for README
+const generateMarkdown = (answers) => {
+  return `# ${answers.project}
+
+  ${generateLicenseBadge(answers.license)}
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [Credits](#credits)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+- [Badges](#badges)
+
+
+## Installation 
+
+To install the necessary dependencies, run the following command: 
+
+${answers.dependencies}
+
+## Usage
+
+${answers.usage}
+
+## License 
+
+This project is licensed under the ${answers.license} license.
+
+## Contributing
+
+${answers.contribution}
+
+## Tests
+
+To run tests, use the following command: 
+
+${answers.tests}
+
+## Questions 
+
+If you have any questions about this project, please reach out:
+- GitHub: https://github.com/${answers.username}
+- Email: ${answers.email}
+
+## Badges
+
+${generateBadges(answers.badges)}`;
+};
 
 module.exports = generateMarkdown;
